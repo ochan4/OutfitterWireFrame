c73b49cb5f6b0e3bee77d489fcf2901a0ed972db
Ayer Chan <ayerchan21@gmail.com>
2016-06-08 16:25:51 +0800

    wireframe update

---
+++ OutfitterWireFrame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OutfitterWireFrame
+//
+//  Created by AierChen on 6/6/16.
+//  Copyright © 2016年 Canterbury Tale Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OutfitterWireFrame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OutfitterWireFrame
+//
+//  Created by AierChen on 6/6/16.
+//  Copyright © 2016年 Canterbury Tale Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
